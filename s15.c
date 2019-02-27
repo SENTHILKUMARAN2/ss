4a572919bd2233129bc59a7a8c28280b55a4c932
SENTHILKUMARAN2 <36395326+SENTHILKUMARAN2@users.noreply.github.com>
2019-02-27 16:31:35 +0530

    Create s15.c

---
+++ s15.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main()
+ {
+     int n;
+printf("enter the no:");
+scanf("%d",&n);
+if(1<=n<=100){
+    printf("median element");
+    }
+    else
+{
+    getch();
+}
+}
