f2d8c669311fa5e907ac876c0348c1990197d229
SENTHILKUMARAN2 <36395326+SENTHILKUMARAN2@users.noreply.github.com>
2019-03-14 19:27:32 +0530

    Update b30.c

--- b30.c
+++ b30.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void main()
 {
-int hr,min,hr1,min1,hr2,min2;
+int hrs,min,hr1,min1,hr2,min2;
 printf("enter 2 times in hour and minute format");
 scanf("%d%d",&hr1,&min1);
 scanf("%d%d",&hr2,&min2);
-hr=hr2-hr1;
+hrs=hr2-hr1;
 min=min2-min1;
-printf("%d\t%d",hr,min);
+printf("%d\t%d",hrs,min);
 }
